41e2ae4f9320f330c7bbbcdbcbd43f87b3f97cf2
Sagar Pratap Singh <sagarpratapsingh9432@gmail.com>
2023-07-13 12:43:41 +0530

    As a User need to enter a valid First Name

--- src/test/java/com/bridgelabz/UserRegistrationTesting.java
+++ src/test/java/com/bridgelabz/UserRegistrationTesting.java
@@ -1,1 +1,26 @@
+package com.bridgelabz;
 
+import org.example.com.bridgelabz.UserRegistration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class UserRegistrationTesting {
+    static UserRegistration userRegistration;
+    @BeforeAll
+    public static void initiate(){
+        userRegistration = new UserRegistration();
+    }
+    @Test
+    public void givenTextWhen_ItContains_FirstCapsIn_FirstName_AndMinimumThreeAlphabet_ShouldReturnTrue(){
+        String actual = userRegistration.validFirstName("Sagar");
+        String expected = "Valid";
+        Assertions.assertSame(actual,expected);
+    }
+    @Test
+    public void givenTextWhen_ItNot_Contains_FirstCapsIn_FirstName_AndMinimumThreeAlphabet_ShouldReturnTrue(){
+        String actual = userRegistration.validFirstName("sa");
+        String expected = "Not Valid";
+        Assertions.assertSame(actual,expected);
+    }
+}
