04906ec33a0fd6a188ae0b344bd851b9720112a4
Sagar Pratap Singh <sagarpratapsingh9432@gmail.com>
2023-07-13 12:31:53 +0530

    Welcomr to User Registration Program with JUnit Testing

---
+++ src/main/java/org/example/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,15 @@
+package org.example.com.bridgelabz;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+    public static String validFirstName(String name){
+        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
+        Matcher matcher = pattern.matcher(name);
+        if (matcher.matches()){
+            return "Valid";
+        }else {
+            return "Not Valid";
+        }
+    }
+}
